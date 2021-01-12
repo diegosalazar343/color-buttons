5e359e61efb213b43da26871c0d0082ce22f97f9
diegosalazar343 <diegosalazar343@gmail.com>
2021-01-12 13:35:38 -0800

    click on paragraph for it to be highlighted
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
   $("button#green").click(function() {
     $("body").removeClass();
     $("body").addClass("green-background");
@@ -12,5 +20,11 @@ $(document).ready(function() {
   $("button#red").click(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
+  });
+
+
+  $("p#highlight").click(function() {
+    $(".change p").removeClass();
+    $(".change p").addClass("highlight");
   });
 });
